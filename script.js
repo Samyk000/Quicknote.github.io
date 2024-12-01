412cb2adf01ababf449029fab360a96209c431d4
Sameer Khan <131415869+Samyk000@users.noreply.github.com>
2024-12-02 00:46:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+/**
+ * Copyright [2024] [Sameer Khan]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 // Select the toggle button and its icon
 const toggleDarkMode = document.getElementById("toggle-dark-mode");
 const iconSpan = toggleDarkMode.querySelector(".icon");
